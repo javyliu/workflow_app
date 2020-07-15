6882679ca34382c02e5e044b852604c4ff420c14
qmliu <qmliu@pearlinpalm.com>
2020-07-15 17:54:21 +0800

    add dice method

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,80 +10,138 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: myCard(),
-    );
+        title: 'Flutter Demo',
+        home: Scaffold(
+          backgroundColor: Colors.red,
+          appBar: AppBar(
+            title: Text("Dicee"),
+            backgroundColor: Colors.red,
+          ),
+          body: DicePage(),
+        ));
   }
 
-  Scaffold myCard() {
-    return Scaffold(
-      backgroundColor: Colors.teal,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage("images/myicon.jpg"),
+  SafeArea myCard() {
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage("images/myicon.jpg"),
+          ),
+          Text(
+            "Javy Liu",
+            style: TextStyle(
+              fontFamily: 'Pacifico',
+              fontSize: 40,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
             ),
-            Text(
-              "Javy Liu",
-              style: TextStyle(
-                fontFamily: 'Pacifico',
-                fontSize: 40,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
+          ),
+          Text(
+            'FLUTTER DEVELOPER',
+            style: TextStyle(
+              fontFamily: 'Source Sans Pro',
+              color: Colors.teal.shade100,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              letterSpacing: 2.5,
             ),
-            Text(
-              'FLUTTER DEVELOPER',
-              style: TextStyle(
-                fontFamily: 'Source Sans Pro',
-                color: Colors.teal.shade100,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                letterSpacing: 2.5,
-              ),
+          ),
+          SizedBox(
+            height: 20,
+            width: 150,
+            child: Divider(
+              color: Colors.teal.shade100,
             ),
-            SizedBox(height: 20,
-              width: 150,
-              child: Divider(color: Colors.teal.shade100,),
-            ),
-            Card(
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-              child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    '+186 0097 9634',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20,
-                    ),
-                  )),
-            ),
-            Card(
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-              child: ListTile(
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+            child: ListTile(
                 leading: Icon(
-                  Icons.email,
+                  Icons.phone,
                   color: Colors.teal,
                 ),
                 title: Text(
-                  'javy_liu@163.com',
+                  '+186 0097 9634',
                   style: TextStyle(
                     color: Colors.teal.shade900,
                     fontFamily: 'Source Sans Pro',
                     fontSize: 20,
                   ),
+                )),
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+            child: ListTile(
+              leading: Icon(
+                Icons.email,
+                color: Colors.teal,
+              ),
+              title: Text(
+                'javy_liu@163.com',
+                style: TextStyle(
+                  color: Colors.teal.shade900,
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20,
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNum = 4;
+  var rightDiceNum = 6;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNum = 5;
+                  print("left button got pressed.=$leftDiceNum");
+                });
+              },
+              child: Image.asset(
+                "images/dice$leftDiceNum.png",
+                width: double.infinity,
+                height: double.infinity,
+                fit: BoxFit.fitWidth,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print("right button get pressed");
+              },
+              child: Image.asset(
+                "images/dice$rightDiceNum.png",
+                width: double.infinity,
+                height: double.infinity,
+                fit: BoxFit.fitWidth,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
